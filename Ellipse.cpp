ff062f8bffef3b9738c306255247f1d3b4f7f60a
markgege <mark24680617@gmail.com>
2023-07-19 17:32:42 -0700

    Added ellipse and rectangle class

---
+++ Ellipse.cpp
@@ -0,0 +1,31 @@
+#include "Ellipse.h"
+void Ellipse::set_rect(const QRect& rect)
+{
+    this->rect = rect;
+}
+
+void Ellipse::draw(const int translate_x, const int translate_y)
+{
+    get_qpainter().setPen(get_pen());
+    get_qpainter().setBrush(get_brush());
+
+    QRect translatedRect(rect.x() + translate_x, rect.y() + translate_y,
+                         rect.width(), rect.height());
+
+    get_qpainter().drawEllipse(translatedRect);
+
+    get_qpainter().restore();
+}
+
+double Ellipse::perimeter() const
+{
+    // Approximation using the Ramanujan's formula
+    double a = rect.width() / 2.0;
+    double b = rect.height() / 2.0;
+    return M_PI * (3 * (a + b) - sqrt((3 * a + b) * (a + 3 * b)));
+}
+
+double Ellipse::area() const
+{
+    return M_PI * (rect.width() / 2.0) * (rect.height() / 2.0);
+}
---
+++ Rectangle.h
@@ -0,0 +1,59 @@
+#ifndef CS1C_2DGRAPH_RECTANGLE_H
+#define CS1C_2DGRAPH_RECTANGLE_H
+
+#include "shape.h"
+
+class Rectangle : public Shape {
+public:
+    /**
+     * @brief constructor of Rectangle
+     * @param device QpaintDevice
+     * @param id id number of shape
+     */
+    Rectangle(QPaintDevice* device = nullptr, int id = -1) : Shape(device, id, shapeType::Rectangle) {}
+
+    /**
+     * @brief Destructor of Rectangle
+     */
+    ~Rectangle() override {}
+
+    /**
+     * @brief set points for shape
+     * @param top_left top left point
+     * @param bottom_right bottom right point
+     */
+    void set_points(const QPoint& top_left, const QPoint& bottom_right);
+
+    /**
+     * @brief draw rectangle using two points
+     * @param translate_x point x
+     * @param translate_y point y
+     */
+    void draw(const int translate_x = 0, const int translate_y = 0) override;
+
+    /**
+     * @brief get perimeter of rectangle
+     * @return perimeter
+     */
+    double perimeter() const override;
+
+    /**
+     * @brief get area of rectangle
+     * @return area
+     */
+    double area() const override;
+
+private:
+    /**
+     * store top left point
+     */
+    QPoint top_left;
+    /**
+     * store bottom right point
+     */
+    QPoint bottom_right;
+
+};
+
+
+#endif //CS1C_2DGRAPH_RECTANGLE_H
