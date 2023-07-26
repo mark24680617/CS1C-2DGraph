d6273f411533cdd24cd3ca054b790594961bdd81
DanielxPrice <danielprice1642@gmail.com>
2023-07-25 23:17:44 -0700

    Comments added for canvas, line, polygon, polyline. Dioxygen

--- canvas.h
+++ canvas.h
@@ -8,9 +8,19 @@
 class canvas : public QWidget
 {
 public:
+    // What: The constructor
+    // Pre-Condition: None
+    // Post-Condition: The object gets constructed
     explicit canvas(QWidget *parent = nullptr);
+
+    // What: Sets Vector
+    // Pre-Condition: None
+    // Post-Condition: The local vector gets updated with the passed in vector
     void setShapes(const vector<Shape*>& shapes);
 protected:
+    // What: Paint Event
+    // Pre-Condition: None
+    // Post-Condition: All shapes get drawn
     void paintEvent(QPaintEvent *event) override;
 private:
     vector<Shape*> shapes;
--- polygon.h
+++ polygon.h
@@ -5,12 +5,34 @@
 class polygon : public Shape
 {
 public:
+    // What: The constructor
+    // Pre-Condition: None
+    // Post-Condition: The object gets constructed with the passed in parameters device and id
     polygon(QPaintDevice* device = nullptr, int id = -1) : Shape{device, id, shapeType::Polygon} {};
+
+    // What: The destructor
+    // Pre-Condition: The object is still in a valid state
+    // Post-Condition: The object gets destructed and put into an invalid state
     ~polygon() override{ }
+
+    // What: Sets the points
+    // Pre-Condition: None
+    // Post-Condition: pushes back the passed in point into the points vector
     void set_point(const QPoint& point);
+
+    // What: Polygon Drawn
+    // Pre-Condition: None
+    // Post-Condition: The polygon gets drawn using the vector of points
     void draw(QPainter& painter, const int translate_x = 0, const int translate_y = 0) override;
 
+    // What: Gets perimeter
+    // Pre-Condition: None
+    // Post-Condition: The perimeter gets returned to caller
     double perimeter() const override;
+
+    // What: Gets area
+    // Pre-Condition: None
+    // Post-Condition: The area gets returned to caller
     double area() const override;
 
 private:
