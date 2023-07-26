8a8a321e2d020a959c885df932aad128df23ecbb
markgege <mark24680617@gmail.com>
2023-07-26 01:06:31 -0700

    add doxygen comment for contact page

--- contact.h
+++ contact.h
@@ -12,20 +12,44 @@ class contact : public QDialog
     Q_OBJECT
 
 public:
+    /**
+     * @brief explicit constructor
+     * @param parent QWidget
+     */
     explicit contact(QWidget *parent = nullptr);
+
+    /**
+     * @brief read from the txt file and display on ui
+     * @param filePath string file
+     */
     void readAndDisplayFile(const QString& filePath);
 
+    /**
+     * @brief destructor
+     */
     ~contact();
 
 private slots:
+    /**
+     * @brief link to github
+     */
     void on_linkgit_clicked();
 
+    /**
+     * @brief link to mainWindow
+     */
     void on_backhome_clicked();
 
+    /**
+     * @brief send comment to txt file
+     */
     void on_sendComment_clicked();
 
 
 private:
+    /**
+     * ui for contact page
+     */
     Ui::contact *ui;
 };
 
