c36ed500ee8e540eb149cc2454374b44889fb207
DanielxPrice <danielprice1642@gmail.com>
2023-07-26 18:53:34 -0700

    comments updated

--- line.h
+++ line.h
@@ -6,15 +6,17 @@
 class Line: public Shape
 {
 public:
-    // What: It constructs an object
-    // Pre-Condition: None
-    // Post-Condition: The object gets constructed with the passed in parameters device and id.
+    /**
+     * @brief constructor of line
+     * @param device QpaintDevice
+     * @param id id number of shape
+     */
     Line(QPaintDevice* device = nullptr, int id = -1): Shape{device, id, shapeType::Line}
     {}
 
-    // What: Destructs the object
-    // Pre-Condition: The object is still in a valid state
-    // Post-Condition: The object gets destructed
+    /**
+     * @brief Destructor of line
+     */
     ~Line() override {}
 
     // What: Sets the points for the line
@@ -22,28 +24,40 @@ public:
     // Post-Condition: arributtes point_begin and point_end get set with the passed in parameters from the vector
     void set_points(const vector<QPoint>& pointsList) override;
 
-    // What: Sets the points for the line
-    // Pre-Condition: None
-    // Post-Condition: arributtes point_begin and point_end get set with the passed in parameters
+    /**
+     * @brief set points for shape
+     * @param top_left top left point
+     * @param bottom_right bottom right point
+     */
     void set_points(const QPoint& point_begin, const QPoint& point_end) override;
 
-    // What: Line Drawn
-    // Pre-Condition: None
-    // Post-Condition: A line gets drawn with two points
+    /**
+     * @brief draw line using two points
+     * @param translate_x point x
+     * @param translate_y point y
+     */
     void draw(QPainter& painter, const int translate_x = 0, const int translate_y = 0) override;
 
-    // What: Gets the perimeter
-    // Pre-Condition: None
-    // Post-Condition: The perimeter of the line gets returned to the caller
+    /**
+     * @brief get perimeter of line
+     * @return perimeter
+     */
     double perimeter() const override;
 
-    // What: Gets the area
-    // Pre-Condition: None
-    // Post-Condition: The area of the line gets returned to the caller
+    /**
+     * @brief get area of line
+     * @return area
+     */
     double area() const override {return 0;}
 
 private:
+    /**
+     * store start point
+     */
     QPoint point_begin;
+    /**
+     * store end point
+     */
     QPoint point_end;
 
 };
